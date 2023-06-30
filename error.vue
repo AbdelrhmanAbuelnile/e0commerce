c58115cb2fbdd159428d46832fbd4f2cfb3035b8
AbdelrhmanAbuelnile <abdohani362@gmail.com>
2023-06-30 19:41:08 +0300

    first commit

---
+++ error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div
+      class="p-10 rounded-3xl flex flex-col justify-center items-center w-3/4 md:w-2/4 bg-Grayish"
+    >
+      <div
+        class="h-2/4 w-3/4 md:w-2/4 md:h-2/6 lg:w-1/4 lg:h-2/4 mx-auto text-center"
+      >
+        <p class="mt-7 text-5xl md:text-7xl font-bold">{{ error.statusCode }}</p>
+        <p class="mt-7 text-3xl md:text-6xl">Ooops.</p>
+        <p class="mt-7">{{ error.message }}</p>
+        <button class="my-7 bg-Primary rounded-md text-white text-xl md:text-3xl p-2 hover:bg-Accent hover:scale-105 duration-300" @click="handleClearError">
+          Go Home...
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(["error"]);
+
+const handleClearError = () => clearError({ redirect: "/" });
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'Primary': '#105E46',
+        'Accent': '#12805D',
+        'Success': '#02D693',
+        'Light': '#BFD1E5',
+        'Tertiary': '#60695C',
+        'Dark': '#1A1F16',
+        'Placeholder': 'rgba(26, 31, 22, 0.5)',
+        'White': '#fff',
+        'Danger': '#E5252C',
+        'Warning': '#E6D117',
+        'Grayish': '#DADADA'
+      }
+    },
+  },
+  plugins: [],
+}
+
